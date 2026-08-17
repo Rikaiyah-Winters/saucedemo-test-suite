4e8e22cbc4ce841f6daa3c7485303a91850fbdd7
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2026-08-16 22:28:14 -0500

    Defined locators for InventoryPage

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -2,6 +2,19 @@ import { Page, Locator, expect } from "@playwright/test";
 
 export class InventoryPage {
     readonly page: Page;
+    readonly hamburgerMenu: Locator;
+    readonly shoppingCartIconLink: Locator;
+    readonly shoppingCartIconNumber: Locator;
+    readonly filter: Locator;
+    //add types of filters
+    //item name locator
+    readonly itemName: Locator;
+    //locator "Add to Cart" button
+    readonly addToCartButton: Locator;
+    //item photo locator
+    readonly itemPhotoLink: Locator;
+
+
 
     constructor(page:Page){
         this.page = page;
