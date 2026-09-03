ed7e2cfaa50f63b31d046d12b723013ad99ade6b
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2026-09-02 21:03:57 -0500

    Split item name and photo leads to details page into 2 tests

--- tests/inventory-sort.spec.ts
+++ tests/inventory-sort.spec.ts
@@ -39,7 +39,9 @@ test.describe("Inventory Page Test Suite", () => {
     test("Hamburger icon leads to proper, working menu", async ({page}) => {});
 
     //each item NAME and PHOTO leads to item details page
-    test("Item name/header and photo leads to item details page", async ({page}) => {});
+    test("Item name/header leads to item details page", async ({page}) => {});
+
+    test("Item photo leads to item details page", async ({page}) => {});
     
     //add to cart button increases cart bage 
     test("Adding an item to cart increases cart badge number", async ({page}) => {});
